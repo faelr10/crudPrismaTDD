48c236c34acdcf765e2b4a7eaafb791190d48273
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-02-04 16:40:26 -0300

    add test for validate cpf and email

---
+++ src/Modules/Client/ClientController.spec.ts
@@ -0,0 +1,13 @@
+
+
+describe('test for ClientController',()=>{
+
+    it('should verified cpf is valid',async()=>{
+
+    })
+
+    it('should verified email is valid',async()=>{
+        
+    })
+
+})
