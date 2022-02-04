739116aa42856ea31249f22b30303fd4d009a182
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-02-04 16:22:36 -0300

    add interface structure

---
+++ src/Modules/Client/structure.ts
@@ -0,0 +1,18 @@
+interface ICreateUser {
+    id?: string
+    name: string
+    cpf: string,
+    email: string
+}
+
+export interface IClientController{
+
+}
+
+export interface IClientService{
+
+}
+
+export interface IClientRepository{
+
+}
