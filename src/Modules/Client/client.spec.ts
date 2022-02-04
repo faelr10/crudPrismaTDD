62bc6ad36b86fe7f8eb50ef433858cc8f25b95f4
Adriano hilario <adrianoferreira31@gmail.com>
2022-02-04 12:02:26 -0300

    changing test create client

--- src/Modules/Client/client.spec.ts
+++ src/Modules/Client/client.spec.ts
@@ -1,27 +1,26 @@
+import { prismaMock } from './../../database/singleton';
+import prisma from '../../database/client';
+
 
 interface CreateUser {
+    id?: string
     name: string
-    cpf:string,
-    email:string
+    cpf: string,
+    email: string
 }
 
-class ClientService{
-    constructor(private clientRepository:ClientRepository ){}
-    async create(data:CreateUser):Promise<object>{
+class ClientService {
+    constructor(private clientRepository: ClientRepository) { }
+    async create(data: CreateUser): Promise<object> {
         const result = await this.clientRepository.create(data)
         return result
     }
 }
 
-class ClientRepository{
-    async create(data:CreateUser):Promise<object>{
-        const newData = {
-            id:'123456',
-            name:data.name,
-            cpf:data.cpf,
-            email:data.email
-        }
-        return newData
+class ClientRepository {
+    async create(data: CreateUser): Promise<object> {
+        const newUser = await prisma.client.create({ data })
+        return newUser
     }
 }
 
@@ -30,21 +29,22 @@ const createSut = () => {
     const sut = new ClientService(clientRepositoryMock)
     return sut
 }
- const createClientRepository = () => {
-    class ClientRepositoryMock extends ClientRepository{}
+const createClientRepository = () => {
+    class ClientRepositoryMock extends ClientRepository { }
     return new ClientRepositoryMock
 }
 
-describe('test client service',()=>{
+describe('test client service', () => {
 
-    it('test create service client',async ()=>{
+    it('test create service client', async () => {
         const sut = createSut()
-        const result = await sut.create({
-            name:'Rafael',
-            cpf:'12055764608',
-            email:'rafaelv.boscato@hotmail.com'
-        })
-        expect(result).toHaveProperty('name','Rafael')
+        const user = {
+            id: '2154122',
+            name: 'Adriano',
+            cpf: '12547889965',
+            email: 'Adriano@Adriano.com'
+        }
+        prismaMock.client.create.mockResolvedValue(user)
+        await expect(sut.create(user)).resolves.toHaveProperty('id')
     })
-
 })
